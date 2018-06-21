90a31f374fed1f830cfd940bc15c434e98dd9a0a
Sumit Goswami <sumit@linkerlogic.com>
2018-06-21 20:51:19 +0530

    - change in pod space

--- Pod/Classes/GooglePlacesSearchController.swift
+++ Pod/Classes/GooglePlacesSearchController.swift
@@ -383,13 +383,11 @@ private class GooglePlacesRequestHelpers {
 
 extension CLLocation {
     func getMile(location:CLLocation) -> String {
-        
         return "\((self.distance(from: location)/1609.34).rounded(toPlaces: 1)) m"
     }
 }
 
 extension Double {
-    /// Rounds the double to decimal places value
     func rounded(toPlaces places:Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
